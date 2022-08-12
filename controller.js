6e4c88bc13692f06f3bf1ee5acfacd343c33083a
Khaled Karim <kskarim177@gmail.com>
2022-08-13 01:41:00 +0600

    new signed in users inserted to db

--- controller.js
+++ controller.js
@@ -1,8 +1,21 @@
-export const userSignup = (req, res) => {
+import User from "./validation.js";
+
+export const userSignup = async (req, res) => {
     try {
+
+        const exists = await User.findOne({ userName: req.body.userName })
+        if (exists) {
+            return res.status(401).json({ message: 'This Username already exists, try a different one' })
+        }
+
+
         const user = req.body;
+        const newUser = new User(user);
+        await newUser.save();
+
+        res.status(200).json({ message: user })
     }
     catch (error) {
-
+        res.status(500).json({ message: error.message })
     }
 }
