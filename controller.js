a9a627836f1c2a138363551f0a8a8b7700c22e02
Khaled Karim <kskarim177@gmail.com>
2022-08-13 13:16:53 +0600

    implemented login system~

--- controller.js
+++ controller.js
@@ -19,3 +19,21 @@ export const userSignup = async (req, res) => {
         res.status(500).json({ message: error.message })
     }
 }
+
+export const userLogin = async (req, res) => {
+    try {
+        const userName = req.body.userName;
+        const password = req.body.password;
+
+        let user = await User.findOne({ userName: userName, password: password });
+        if (user) {
+            return res.status(200).json({ data: user })
+        }
+        else {
+            return res.status(401).json('Invalid Login')
+        }
+    }
+    catch (error) {
+        res.status(500).json('Error', error.message)
+    }
+}
